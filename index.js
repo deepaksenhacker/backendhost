4ace054031bba89750c8806fff27f8e5584406b1
deepaksenhacker <care.deepaksen@gmail.com>
2024-08-23 07:33:07 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/hi',(req,res)=>{
     res.json({msg:'hi deepak'});
 })
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
