3ac57b074d002b24257bc3e595c12a815366257f
deepaksenhacker <care.deepaksen@gmail.com>
2024-08-23 07:37:02 +0530

    hi

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/hi',(req,res)=>{
     res.json({msg:'hi deepak'});
 })
-
+app.get('/data',(req,res)=>{
+    res.json({data : 'data is here'})
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
